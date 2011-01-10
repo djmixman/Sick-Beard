b62a9d1abb0649ee4e5b65f2417f7666a7f351a5
djmixman <djmixman@gmail.com>
2011-01-10 00:09:21 -0600

    Trying to add CHMOD on directory create...

--- sickbeard/postProcessor.py
+++ sickbeard/postProcessor.py
@@ -623,6 +623,8 @@ class PostProcessor(object):
             self._log(u"Season folder didn't exist, creating it", logger.DEBUG)
             try:
                 ek.ek(os.mkdir, dest_path)
+# Gonna try this / mix-man
+		ek.ek(os.chmod, dest_path, 777)
             except OSError, IOError:
                 raise exceptions.PostProcessingFailed("Unable to create the episode's destination folder: "+str(dest_path))
 
